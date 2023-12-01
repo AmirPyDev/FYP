e4e2e2bb1397b6353b58868b46912612da52c054
AmirPyDev <142138168+AmirPyDev@users.noreply.github.com>
2023-12-01 23:05:27 +0500

    add models customer product order

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class customer(models.Model):
+	name = models.CharField(max_length=200, null = True)
+	phone = models.CharField(max_length=200, null = True)
+	email = models.CharField(max_length=200, null = True)
+	date_created = models.DateTimeField(auto_now_add=True, null = True)
+
+	def __str__(self):
+		return self.name
+
+class products(models.Model):
+	CATEGORY = (
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			) 
+
+	name = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	description = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class order(models.Model):
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
+
+	# customer
+	# products
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+
+
+
+
+
